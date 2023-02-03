91051fbd120ae06921ffe4f0ec43adfb48b59d0e
Hennry <tanminkwan@gmail.com>
2023-02-03 08:26:47 +0000

    modified

--- app/static/js/myjs.js
+++ app/static/js/myjs.js
@@ -1,9 +1,21 @@
 /* */
+function removeTags(str) {
+	if ((str===null) || (str===''))
+		return false;
+	else
+		str = str.toString();
+		
+	// Regular expression to identify HTML tags in
+	// the input string. Replacing the identified
+	// HTML tag with a null string.
+	return str.replace( /(<([^>]+)>)/ig, '');
+}
+
 function viewDict(dict) {
 
     if(typeof toastr == 'undefined')return;
 
-    var word = dict.innerHTML
+    var word = removeTags(dict.innerHTML)
     var rlist = getDict(word);
     var deco_word = '<span class="dict-word">'+word+'</span>'
     
